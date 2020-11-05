58266b884fadbb1bde51d7be675d12157b6c6021
Sidharth badal <siddharthabadal.2015@gmail.com>
2020-11-06 00:48:38 +0530

    Update googlenews.py

--- googlenews.py
+++ googlenews.py
@@ -15,3 +15,6 @@ keyword = input("Enter the country or keyword: ")
 todaynews = todaysNews(keyword)
 for news in todaynews:
     print(news)
+    
+    
+# we can directly return googlenews.result and this will show complete news with title, link, description and dates.
